d5876054d17a5a91f4fca6daa8079ea82c3a7312
Ange Yaghi <me@angeyaghi.com>
2022-10-28 01:17:00 -0400

    Updated demo 1 to support multiple monitors

--- demos/01_window_spawning_demo/src/main.cpp
+++ demos/01_window_spawning_demo/src/main.cpp
@@ -1,5 +1,47 @@
 #include <palace-engine/include/platform/entry_point.hpp>
 
+#include <sstream>
+
 int palaceMain(palace::ApplicationContext *context) {
+    constexpr int WindowWidth = 256;
+    constexpr int WindowHeight = 256;
+
+    palace::WindowManager *windowManager = context->windowManager();
+    windowManager->updateDisplayDevices();
+    const size_t displayDeviceCount = windowManager->displayDeviceCount();
+    if (displayDeviceCount == 0) {
+        return -1;
+    }
+
+    palace::DynamicArray<palace::Window *> windows;
+    for (size_t i = 0; i < displayDeviceCount; ++i) {
+        palace::DisplayDevice *displayDevice = windowManager->getDisplayDevice(i);
+
+        std::stringstream ss;
+        ss << "Window " << i << " | Palace Engine | Demo 01 | Window Spawning Demo";
+
+        palace::Window::Parameters parameters;
+        parameters.style = palace::Window::Style::Windowed;
+        parameters.title = ss.str();
+        parameters.size = { WindowWidth, WindowHeight };
+        parameters.position = {
+            displayDevice->origin().x() + displayDevice->logicalSize().w() / 2 - WindowWidth / 2,
+            displayDevice->origin().y() + displayDevice->logicalSize().h() / 2 - WindowHeight / 2 };
+        windows.append(windowManager->spawnWindow(parameters));
+    }
+
+    bool windowsRemaining = true;
+    while (windowsRemaining) {
+        windowsRemaining = false;
+        for (size_t i = 0; i < windows.size(); ++i) {
+            if (windows[i]->isOpen()) {
+                windowsRemaining = true;
+                break;
+            }
+        }
+
+        windowManager->processMessages();
+    }
+
     return 0;
 }
